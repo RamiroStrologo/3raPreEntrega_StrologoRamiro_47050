333cf75c37ed2fa2a0daaed34d12b1fa677a3e73
Ramiro <ramirostrologo@gmail.com>
2023-09-30 17:47:03 -0300

    implementacion de js

---
+++ js/app.js
@@ -0,0 +1,377 @@
+//Objeto con claves que identifica a los juegos por consola.
+const juegosPorConsola = {
+  ps3: [
+    {
+      imagenSrc: "../assets/images/webP/PortadasPS3/the-last-of-us-ps3.webp",
+      titulo: "The Last of Us",
+      genero: "Acción y aventura",
+      precio: "$6000",
+      descripcion:
+        "Únete a Joel y Ellie en un emotivo viaje a través de un mundo postapocalíptico mientras luchan por sobrevivir y encontrar la esperanza en este aclamado juego.",
+    },
+    {
+      imagenSrc: "../assets/images/webP/PortadasPS3/gta-v-ps3.webp",
+      titulo: "GTA V",
+      genero: "Mundo abierto, acción",
+      precio: "$5500",
+      descripcion:
+        "Explora el vibrante mundo de Los Santos en esta entrega épica de la saga Grand Theft Auto, llena de acción, misiones y actividades.",
+    },
+    {
+      imagenSrc:
+        "../assets/images/webP/PortadasPS3/the-elder-scrolls-v-skyrim-ps3.webp",
+      titulo: "The Elder Scrolls V: Skyrim",
+      genero: "RPG de mundo abierto",
+      precio: "$4500",
+      descripcion:
+        "Sumérgete en el vasto mundo de Skyrim, repleto de dragones, magia y aventuras épicas mientras forjas tu propio camino como el Sangre de Dragón.",
+    },
+    {
+      imagenSrc: "../assets/images/webP/PortadasPS3/battlefield-4-ps3.webp",
+      titulo: "Battlefield 4",
+      genero: "Shooter en primera persona",
+      precio: "$4000",
+      descripcion:
+        "Entra en batallas intensas y combates realistas en este juego de disparos multijugador ambientado en diversos escenarios de guerra.",
+    },
+    {
+      imagenSrc: "../assets/images/webP/PortadasPS3/dark-souls-ii.webp",
+      titulo: "Dark Souls II",
+      genero: "Acción y rol",
+      precio: "$5000",
+      descripcion:
+        "Enfréntate a desafíos brutales y enemigos mortales en un mundo oscuro y misterioso en este juego de rol de acción.",
+    },
+    {
+      imagenSrc: "../assets/images/webP/PortadasPS3/dead-space-ps3.webp",
+      titulo: "Dead Space",
+      genero: "Survival horror",
+      precio: "$3500",
+      descripcion:
+        "Sumérgete en una experiencia aterradora mientras te enfrentas a horrores alienígenas en el espacio profundo en esta entrega de la serie Dead Space.",
+    },
+    {
+      imagenSrc:
+        "../assets/images/webP/PortadasPS3/metal-gear-rising-revengeance-ps3.webp",
+      titulo: "Metal Gear Rising: Revengeance",
+      genero: "Acción y hack and slash",
+      precio: "$3000",
+      descripcion:
+        "Controla a Raiden en este juego de acción frenético donde la espada es tu herramienta principal para enfrentarte a enemigos poderosos.",
+    },
+    {
+      imagenSrc:
+        "../assets/images/webP/PortadasPS3/god-of-war-ascension-ps3.webp",
+      titulo: "God of War: Ascension",
+      genero: "Acción y aventura",
+      precio: "$4200",
+      descripcion:
+        "Únete a Kratos en su búsqueda de redención y venganza en este emocionante capítulo de la saga God of War.",
+    },
+    {
+      imagenSrc:
+        "../assets/images/webP/PortadasPS3/metal-gear-solid-4-guns-of-the-patriots-ps3.webp",
+      titulo: "Metal Gear Solid 4: Guns of the Patriots",
+      genero: "Acción y sigilo",
+      precio: "$3800",
+      descripcion:
+        "Vive la última misión de Solid Snake en un mundo lleno de conspiraciones y giros sorprendentes en esta entrega de Metal Gear Solid.",
+    },
+    {
+      imagenSrc:
+        "../assets/images/webP/PortadasPS3/need-for-speed-hot-pursuit-ps3.webp",
+      titulo: "Need for Speed: Hot Pursuit",
+      genero: "Carreras y mundo abierto",
+      precio: "$3000",
+      descripcion:
+        "Vive la emoción de las carreras ilegales y la persecución policial en este juego de la serie Need for Speed.",
+    },
+    {
+      imagenSrc: "../assets/images/webP/PortadasPS3/far-cry-4-_1_.webp",
+      titulo: "Far Cry 4",
+      genero: "Mundo abierto, disparos",
+      precio: "$4200",
+      descripcion:
+        "Explora la región de Kyrat y enfréntate a un régimen opresivo en esta entrega de la saga Far Cry llena de acción y aventuras.",
+    },
+    {
+      imagenSrc:
+        "../assets/images/webP/PortadasPS3/silent-hill-downpour-_1_.webp",
+      titulo: "Silent Hill: Downpour",
+      genero: "Survival horror",
+      precio: "$2800",
+      descripcion:
+        "Sumérgete en una pesadilla aterradora mientras exploras la misteriosa ciudad de Silent Hill en este juego de supervivencia.",
+    },
+  ],
+  ps4: [
+    {
+      imagenSrc:
+        "../assets/images/webP/portadasPS4/crash-bandicoot-4-its-about-time-ps4.webp",
+      titulo: "Crash Bandicoot 4: It's About Time",
+      genero: "Plataforma",
+      precio: "$7000",
+      descripcion:
+        "Únete a Crash Bandicoot y Coco en una aventura a través de distintas dimensiones mientras luchan para evitar que los villanos Neo Cortex y Nefarious Tropy conquisten el multiverso.",
+    },
+    {
+      imagenSrc: "../assets/images/webP/portadasPS4/cyberpunk-2077-ps4.webp",
+      titulo: "Cyberpunk 2077",
+      genero: "RPG de acción",
+      precio: "Precio no especificado",
+      descripcion:
+        "Sumérgete en el mundo de Night City en este juego de rol de acción ambientado en un futuro distópico.",
+    },
+    {
+      imagenSrc: "../assets/images/webP/portadasPS4/bloodborne-ps4.webp",
+      titulo: "Bloodborne",
+      genero: "Acción de rol",
+      precio: "Precio no especificado",
+      descripcion:
+        "Embárcate en una aventura oscura y desafiante en una ciudad infestada de bestias y horrores sobrenaturales.",
+    },
+    {
+      imagenSrc: "../assets/images/webP/portadasPS4/hogwarts-legacy-ps4.webp",
+      titulo: "Hogwarts Legacy",
+      genero: "RPG de mundo abierto",
+      precio: "Precio no especificado",
+      descripcion:
+        "Explora el mundo mágico de Hogwarts y descubre secretos, aprende hechizos y enfrenta criaturas mientras desentrañas misterios ancestrales.",
+    },
+    {
+      imagenSrc:
+        "../assets/images/webP/portadasPS4/resident-evil-village-ps4.webp",
+      titulo: "Resident Evil Village",
+      genero: "Survival horror",
+      precio: "Precio no especificado",
+      descripcion:
+        "Sumérgete en un pueblo aterrorizante y enfrenta horrores inimaginables mientras buscas a tu hija secuestrada en esta última entrega de la saga Resident Evil.",
+    },
+    {
+      imagenSrc: "../assets/images/webP/portadasPS4/nioh-ps4.webp",
+      titulo: "Nioh",
+      genero: "Acción y rol",
+      precio: "$5000",
+      descripcion:
+        "Únete a William Adams en su lucha contra espíritus malignos y demonios en el Japón feudal en esta emocionante mezcla de acción y rol.",
+    },
+    {
+      imagenSrc: "../assets/images/webP/portadasPS4/death-stranding-ps4.webp",
+      titulo: "Death Stranding",
+      genero: "Acción y exploración",
+      precio: "Precio no especificado",
+      descripcion:
+        "Embárcate en un viaje surrealista a través de un mundo devastado, entregando suministros y reconectando a la humanidad en un entorno postapocalíptico.",
+    },
+    {
+      imagenSrc: "../assets/images/webP/portadasPS4/doom-ps4.webp",
+      titulo: "DOOM",
+      genero: "Acción y disparos",
+      precio: "Precio no especificado",
+      descripcion:
+        "Enfréntate a hordas de demonios del infierno en una experiencia frenética llena de acción y disparos despiadados.",
+    },
+    {
+      imagenSrc: "../assets/images/webP/portadasPS4/prey-ps4.webp",
+      titulo: "Prey",
+      genero: "Acción y suspense",
+      precio: "Precio no especificado",
+      descripcion:
+        "Explora una estación espacial abandonada infestada de amenazas alienígenas mientras descubres los oscuros secretos de Talos I.",
+    },
+    {
+      imagenSrc: "../assets/images/webP/portadasPS4/elden-ring.webp",
+      titulo: "Elden Ring",
+      genero: "Acción de mundo abierto",
+      precio: "Precio no especificado",
+      descripcion:
+        "Adéntrate en un vasto mundo lleno de peligros y maravillas en esta colaboración épica entre Hidetaka Miyazaki y George R. R. Martin.",
+    },
+    {
+      imagenSrc:
+        "../assets/images/webP/portadasPS4/call-of-duty-black-ops-cold-war-ps4.webp",
+      titulo: "Call of Duty: Black Ops Cold War",
+      genero: "Shooter de acción",
+      precio: "Precio no especificado",
+      descripcion:
+        "Sumérgete en operaciones encubiertas y enfrentamientos intensos en la Guerra Fría mientras persigues a un misterioso espía soviético.",
+    },
+    {
+      imagenSrc:
+        "../assets/images/webP/portadasPS4/assassins-creed-valhalla-ps4.webp",
+      titulo: "Assassin's Creed Valhalla",
+      genero: "Acción y aventura",
+      precio: "Precio no especificado",
+      descripcion:
+        "Explora la Edad Media como un feroz vikingo, saquea y conquista en este épico viaje de la saga Assassin's Creed.",
+    },
+  ],
+  xbox: [
+    {
+      imagenSrc: "../assets/images/webP/portadasXONE/quantum-break-xone.webp",
+      titulo: "Quantum Break",
+      genero: "Acción y aventura",
+      precio: "Precio no especificado",
+      descripcion:
+        "Disfruta de un emocionante juego de acción y aventuras con elementos de manipulación del tiempo en Quantum Break.",
+    },
+    {
+      imagenSrc:
+        "../assets/images/webP/portadasXONE/assassins-creed-odyssey-xone.jpg",
+      titulo: "Assassin's Creed Odyssey",
+      genero: "Mundo abierto, acción",
+      precio: "Precio no especificado",
+      descripcion:
+        "Embárcate en una épica odisea en el mundo antiguo de Grecia mientras tomas decisiones que afectarán el destino en Assassin's Creed Odyssey.",
+    },
+    {
+      imagenSrc: "../assets/images/webP/portadasXONE/battlefield-1-xone.webp",
+      titulo: "Battlefield 1",
+      genero: "Shooter en primera persona",
+      precio: "Precio no especificado",
+      descripcion:
+        "Sumérgete en intensas batallas de la Primera Guerra Mundial en Battlefield 1.",
+    },
+    {
+      imagenSrc:
+        "../assets/images/webP/portadasXONE/call-of-duty-black-ops-iii-xone.webp",
+      titulo: "Call of Duty: Black Ops III",
+      genero: "Shooter en primera persona",
+      precio: "Precio no especificado",
+      descripcion:
+        "Enfréntate a operaciones encubiertas y combates frenéticos en Call of Duty: Black Ops III.",
+    },
+    {
+      imagenSrc:
+        "../assets/images/webP/portadasXONE/grand-theft-auto-v-xone.webp",
+      titulo: "Grand Theft Auto V",
+      genero: "Mundo abierto, acción",
+      precio: "Precio no especificado",
+      descripcion:
+        "Explora Los Santos y sumérgete en una historia llena de crimen y personajes en Grand Theft Auto V.",
+    },
+    {
+      imagenSrc:
+        "../assets/images/webP/portadasXONE/the-witcher-3-wild-hunt-xone.webp",
+      titulo: "The Witcher 3: Wild Hunt",
+      genero: "RPG de mundo abierto",
+      precio: "Precio no especificado",
+      descripcion:
+        "Embárcate en una búsqueda épica como Geralt de Rivia en The Witcher 3: Wild Hunt.",
+    },
+    {
+      imagenSrc:
+        "../assets/images/webP/portadasXONE/forza-motorsport-5-xone.webp",
+      titulo: "Forza Motorsport 5",
+      genero: "Carreras",
+      precio: "Precio no especificado",
+      descripcion:
+        "Experimenta la emoción de las carreras y los autos de lujo en Forza Motorsport 5.",
+    },
+    {
+      imagenSrc:
+        "../assets/images/webP/portadasXONE/gears-of-war-ultimate-edition-xone.webp",
+      titulo: "Gears of War: Ultimate Edition",
+      genero: "Acción y disparos",
+      precio: "Precio no especificado",
+      descripcion:
+        "Lucha contra hordas de enemigos en este remasterizado clásico, Gears of War: Ultimate Edition.",
+    },
+    {
+      imagenSrc:
+        "../assets/images/webP/portadasXONE/star-wars-battlefront-xone.webp",
+      titulo: "Star Wars Battlefront",
+      genero: "Shooter en primera persona",
+      precio: "Precio no especificado",
+      descripcion:
+        "Únete a las batallas icónicas de Star Wars en Star Wars Battlefront.",
+    },
+    {
+      imagenSrc: "../assets/images/webP/portadasXONE/dark-souls-iii-xone.webp",
+      titulo: "Dark Souls III",
+      genero: "Acción y rol",
+      precio: "Precio no especificado",
+      descripcion:
+        "Enfréntate a desafíos y monstruos despiadados en la oscura y fascinante aventura de Dark Souls III.",
+    },
+    {
+      imagenSrc: "../assets/images/webP/portadasXONE/the-evil-within-xone.webp",
+      titulo: "The Evil Within",
+      genero: "Survival horror",
+      precio: "Precio no especificado",
+      descripcion:
+        "Sumérgete en un mundo lleno de pesadillas y horrores en The Evil Within.",
+    },
+    {
+      imagenSrc:
+        "../assets/images/webP/portadasXONE/ryse-son-of-rome-xone.webp",
+      titulo: "Ryse: Son of Rome",
+      genero: "Acción y aventura",
+      precio: "Precio no especificado",
+      descripcion:
+        "Embárcate en una emocionante aventura romana llena de combates y emociones en Ryse: Son of Rome.",
+    },
+  ],
+};
+
+const btnPS3 = document.querySelector("#btnPS3");
+btnPS3.addEventListener("click", mostrarJuegosPS3);
+
+function mostrarJuegosPS3() {
+  const gamesContainer = document.querySelector("#section_juegos_tienda");
+  //CREO EL SEPARADOR, ASIGNO CLASES, INICIALIZO ATRIBUTOS Y RELACIONO LOS ELEMENTOS
+  const separator = document.createElement("div");
+  const imgSep1 = document.createElement("img");
+  const imgSep2 = document.createElement("img");
+  const textSep = document.createElement("h2");
+  separator.classList.add(
+    "mt-3",
+    "mb-2",
+    "h2_tienda",
+    "d-flex",
+    "justify-content-evenly"
+  );
+  separator.id = "ps3_games";
+  imgSep1.src = "../assets/images/nav_icos/ps-logo-ico.svg";
+  imgSep1.alt = "Icono PS";
+  imgSep2.src = "../assets/images/nav_icos/ps-logo-ico.svg";
+  imgSep2.alt = "Icono PS";
+  textSep.innerText = "JUEGOSPS3";
+  separator.appendChild(imgSep1);
+  separator.appendChild(textSep);
+  separator.appendChild(imgSep2);
+  gamesContainer.appendChild(separator);
+  //RECORRE EL ARREGLO DE JUEGOS
+  const childGamesContainer = document.createElement("div");
+  juegosPorConsola.ps3.forEach((juego) => {
+    //CREA LOS ELEMENTOS
+    const divGame = document.createElement("div");
+    const imgGame = document.createElement("img");
+    const parrafo = document.createElement("p");
+    // ASIGNA LAS CLASES E INICIALIZA ATRIBUTOS
+    childGamesContainer.classList.add(
+      "d-flex",
+      "flex-wrap",
+      "justify-content-start",
+      "contenedor_portadas_tienda"
+    );
+    divGame.classList.add(
+      "d-flex",
+      "flex-wrap",
+      "flex-column",
+      "mb-3",
+      "div_img_tienda"
+    );
+    imgGame.classList.add("img_portadas");
+    imgGame.src = juego.imagenSrc;
+    imgGame.alt = juego.titulo;
+    parrafo.innerHTML = `Género: ${juego.genero} <br />
+    Precio: ${juego.precio} <br />
+    Descripción: ${juego.descripcion}`;
+    //EMPARENTA LOS ELEMENTOS
+    divGame.appendChild(imgGame);
+    divGame.appendChild(parrafo);
+    childGamesContainer.appendChild(divGame);
+  });
+  gamesContainer.appendChild(childGamesContainer);
+}
